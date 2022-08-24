289adb2a436539525238fd7ee00254e525215acc
Adnane <nanitosse@gmail.com>
2022-08-24 18:46:28 -0400

    Navigation

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,11 +1,55 @@
-import { useState } from "react";
-import {CAMPSITES} from '../shared/campsites';
+// import { useState } from "react";
+// import {CAMPSITES} from '../shared/campsites';
 import DirectoryScreen from './DirectoryScreen';
+import { Platform, View  } from 'react-native';
+import Constants from 'expo-constants';
+import { createStackNavigator } from '@react-navigation/stack';
+import CampsiteInfoScreen from './CampsiteInfoScreen';
 
-const Main = ()=>{
-    const [campsites, setCampsites] = useState(CAMPSITES);
-    return <DirectoryScreen campsites={campsites}/>;
+
+
+const DirectoryNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator
+            initialRouteName='Directory'
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#5637DD'
+                },
+                headerTinColor: '#fff'
+
+            }}
+        >
+            <Stack.Screen
+                name='Directory'
+                component={DirectoryScreen}
+                options={{ title: 'Campsite Diorectory' }}
+            />
+            <Stack.Screen
+                name='CampsiteInfo'
+                component={CampsiteInfoScreen}
+                options={({route}) => ({
+                    title: route.params.campsite.name
+
+                })}
+            />
+
+
+
+        </Stack.Navigator>
+    )
+}
+
+
+const Main = () => {
+    // const [camp, setCampsites] = useState(CAMPSITES);
+    return (
+        <View style={{ flex: 1, paddingTop:Platform.OS==='ios'? 0 :Constants.StatusBarHeight }}>
+            <DirectoryNavigator /> 
+        </View>
+    );
 
 }
 
-export default Main
+export default Main;
